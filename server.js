e45bf08d84cd8df6ca77e1d9915463e981b013bb
AniElaina <anicook13@gmail.com>
2021-07-26 15:53:39 -0400

    saving changes mid lesson1

---
+++ server.js
@@ -0,0 +1,33 @@
+const { animals } = require("./data/animals.json")
+
+const express = require("express");
+
+const app = express();    ///these two steps instantiate the server then tell it to listen for requests 
+
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+// adding the route here /// 
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log("API server now on port 3001!");
+});
+
