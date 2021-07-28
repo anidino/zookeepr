0b4da01318ade09a13d2dcd5ee5f41e4dcc78dc6
AniElaina <anicook13@gmail.com>
2021-07-28 06:26:04 -0400

    saving changes after 11.2.6

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require("express");
 
 const { animals } = require("./data/animals")
@@ -5,6 +8,11 @@ const { animals } = require("./data/animals")
 const PORT = process.env.PORT || 3001;
 
 const app = express();    ///these two steps instantiate the server then tell it to listen for requests 
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }))  // Express.js method that takes incoming POST data and converts to key/value parirings that can be accssed in the req.body object. 
+// parse incoming JSON data
+app.use(express.json()); // the express.json method takes incoming POST data in JSON form and parses it into the req.body 
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -50,6 +58,21 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+
+    // our function's main code will go here!
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, "./data/animals.json"),
+        JSON.stringify({ animals: animalsArray }, null, 2)  // the null argument means we don't want to edit any of our existing data. If we did, we could pass something in there 
+    );
+
+
+    // return finished code to post route for response
+    return animal;
+}
+
 
 // adding the routes here /// 
 app.get('/api/animals', (req, res) => {
@@ -70,10 +93,38 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();  // req.body property is where we can access that data on the server side and do something with it
+
+    // if any data in req.body is incorrect, send 400 error back 
+    if (!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted");  // res.status().send(); is a response method to relay a message to the client making a request. We send them an http status code and message explaining what went wrong. 400 range means user error not server error... 
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+
+    }
+
+
 });
+
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}!`);
